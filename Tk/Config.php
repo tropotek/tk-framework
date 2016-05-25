3802e5d38a299f6421eb718015b09e7b1ec9c111
tropotek <info@tropotek.com>
2016-05-25 10:19:23 +1000

    finished basic HTTP objects

--- Tk/Config.php
+++ Tk/Config.php
@@ -73,6 +73,10 @@ namespace Tk;
  * @license Copyright 2007 Michael Mifsud
  * @deprecated Will consider using a flat array and some sort of initilasation static method somewhere (A helper maybe???)
  *  -- See what other frameworks are doing for their config system, TIP: keep it simple....
+ * This object has been used both as a place for global site settings and also a DI container
+ * it may be time to seperate these responsibilities and use some pattern that is more appropriate...???
+ * I will leave it here for now but know that only the application level should be using this object nothing in the libs 
+ * should be implementing this in the future...
  */
 class Config extends ArrayObject
 {
