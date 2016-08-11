8d3a3662f9d8df859c46ff3e32e4e5a2484ac026
tropotek <info@tropotek.com>
2016-08-11 13:20:39 +1000

    Modified user management in administration

--- Tk/Log/MonologLineFormatter.php
+++ Tk/Log/MonologLineFormatter.php
@@ -14,6 +14,8 @@ class MonologLineFormatter extends LineFormatter
 {
     const APP_FORMAT = "[%datetime%]%pre% %channel%.%level_name%: %message% %context% %extra%\n";
 
+    protected $scriptTime = 0;
+
     /**
      * @param string $format                     The format of the message
      * @param string $dateFormat                 The format of the timestamp: one supported by DateTime::format
@@ -22,6 +24,7 @@ class MonologLineFormatter extends LineFormatter
      */
     public function __construct($format = null, $dateFormat = 'Y-m-d H:i:s', $allowInlineLineBreaks = true, $ignoreEmptyContextAndExtra = true)
     {
+        $this->scriptTime = microtime(true);
         $format = $format ?: static::APP_FORMAT;
         parent::__construct($format, $dateFormat, $allowInlineLineBreaks, $ignoreEmptyContextAndExtra);
     }
@@ -33,42 +36,28 @@ class MonologLineFormatter extends LineFormatter
     {
         $output = parent::format($record);
 
-        $pre = sprintf('[%5.2f][%8s]', round(\Tk\Config::scriptDuration(), 2), self::bytes2String(memory_get_usage(false)));
+        $pre = sprintf('[%5.2f][%8s]', round($this->scriptDuration(), 2), \Tk\File::bytes2String(memory_get_usage(false)));
         $output = str_replace('%pre%', $pre, $output);
 
         return $output;
     }
 
     /**
-     * Convert a value from bytes to a human readable value
-     *
-     * @param int $bytes
-     * @return string
-     * @author http://php-pdb.sourceforge.net/samples/viewSource.php?file=twister.php
+     * @param $t
      */
-    static function bytes2String($bytes, $round = 2)
+    public function setScripTime($t)
     {
-        $tags = array('b', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-        $index = 0;
-        while ($bytes > 999 && isset($tags[$index + 1])) {
-            $bytes /= 1024;
-            $index++;
-        }
-        $rounder = 1;
-        if ($bytes < 10) {
-            $rounder *= 10;
-        }
-        if ($bytes < 100) {
-            $rounder *= 10;
-        }
-        $bytes *= $rounder;
-        settype($bytes, 'integer');
-        $bytes /= $rounder;
-        if ($round > 0) {
-            $bytes = round($bytes, $round);
-            return sprintf('%.' . $round . 'f %s', $bytes, $tags[$index]);
-        } else {
-            return sprintf('%s %s', $bytes, $tags[$index]);
-        }
+        $this->scriptTime = $t;
     }
+
+    /**
+     * Get the current script running time in seconds
+     *
+     * @return string
+     */
+    public function scriptDuration()
+    {
+        return (string)(microtime(true) - $this->scriptTime);
+    }
+
 }
