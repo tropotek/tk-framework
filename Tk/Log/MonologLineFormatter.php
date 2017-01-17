ea69ffc8da334020f7331a4f094af20a9b327a28
tropotek <info@tropotek.com>
2017-01-17 13:23:18 +1100

    finalising Table column select action

--- Tk/Log/MonologLineFormatter.php
+++ Tk/Log/MonologLineFormatter.php
@@ -22,7 +22,7 @@ class MonologLineFormatter extends LineFormatter
      * @param bool   $allowInlineLineBreaks      Whether to allow inline line breaks in log entries
      * @param bool   $ignoreEmptyContextAndExtra
      */
-    public function __construct($format = null, $dateFormat = 'Y-m-d H:i:s', $allowInlineLineBreaks = true, $ignoreEmptyContextAndExtra = true)
+    public function __construct($format = null, $dateFormat = 'H:i:s.u', $allowInlineLineBreaks = true, $ignoreEmptyContextAndExtra = true)
     {
         $this->scriptTime = microtime(true);
         $format = $format ?: static::APP_FORMAT;
@@ -34,10 +34,29 @@ class MonologLineFormatter extends LineFormatter
      */
     public function format(array $record)
     {
+
+        $colors = array(
+            'emergency' => 'red',
+            'alert' => 'light_cyan',
+            'critical' => 'light_red',
+            'error' => 'light_red',
+            'warning' => 'yellow',
+            'notice' => 'light_purple',
+            'info' => 'white',
+            'debug' => 'light_gray'
+        );
+
+        //error_log(print_r($record, true));
+        $levelName = $record['level_name'];
+        $record['level_name'] = $levelName[0];
+        $record['message'] = \Tk\Color::getCliString($record['message'], $colors[strtolower($levelName)]);
+
         $output = parent::format($record);
-        $pre = sprintf('[%5.2f][%9s]', round($this->scriptDuration(), 2), \Tk\File::bytes2String(memory_get_usage(false)));
+        //$pre = sprintf('[%5.2f][%9s]', round($this->scriptDuration(), 2), \Tk\File::bytes2String(memory_get_usage(false)));
+        $pre = sprintf('[%8s]', \Tk\File::bytes2String(memory_get_usage(false)));
         $output = str_replace('%post%', $pre, $output);
 
+        //return \Tk\Color::getCliString($output, 'white');
         return $output;
     }
 
