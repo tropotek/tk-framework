2651cd88d37fae074d017a467237be77b69ab539
tropotek <info@tropotek.com>
2017-05-03 14:43:35 +1000

    Minor Code Updates - dev

---
+++ Tk/CurlyTemplate.php
@@ -0,0 +1,230 @@
+<?php
+namespace Tk;
+
+/**
+ * 
+ * This is a curly braces style template parser.
+ * 
+ * Note: Strings will replace all '{paramName}' curly params in the template
+ *       However blocks can be use for repeating or choice elements '{blockName}...{/blockName}'
+ *
+ * Template Example:
+ * <code>
+ *
+ *  <html>
+ *    <head>
+ *      <title>{headTitle}</title>
+ *    </head>
+ *    <body>
+ *      <h1>{pageTitle}</h1>
+ *      <table>
+ *        {rowBlock}
+ *          <tr>
+ *          {dataBlock}
+ *            <td>{dataValue}</td>
+ *          {/dataBlock}
+ *          </tr>
+ *        {/rowBlock}
+ *      </table>
+ *    </body>
+ *  </html>
+ *
+ * </code>
+ *
+ * 
+ * 
+ * Data Example 1: Using nested arrays for repeating blocks
+ * 
+ * <code>
+ * 
+ *   $tpl->parse(array(
+ *     'headTitle' => 'This is the Head Title.',
+ *     'pageTitle' => 'This is the main page title.',
+ *     
+ *     'rowBlock' => array(
+ *       array(
+ *         'dataBlock' => array(
+ *           array('dataValue' => 'dataValue1.1'),
+ *           array('dataValue' => 'dataValue1.2'),
+ *           array('dataValue' => 'dataValue1.3')
+ *         )
+ *       ),
+ *       array(
+ *         'dataBlock' => array(
+ *           array('dataValue' => 'dataValue2.1'),
+ *           array('dataValue' => 'dataValue2.2'),
+ *           array('dataValue' => 'dataValue2.3')
+ *         )
+ *       ),
+ *       array(
+ *         'dataBlock' => array(
+ *           array('dataValue' => 'dataValue3.1'),
+ *           array('dataValue' => 'dataValue3.2'),
+ *           array('dataValue' => 'dataValue3.3')
+ *         )
+ *       )
+ *     ),
+ *     'listBlock' => array(
+ *         array('linkUrl' => 'link1.html', 'linkText' => 'Link 1'),
+ *         array('linkUrl' => 'link2.html', 'linkText' => 'Link 2'),
+ *         array('linkUrl' => 'link3.html', 'linkText' => 'Link 3'),
+ *         array('linkUrl' => 'link4.html', 'linkText' => 'Link 4')
+ *       )
+ *     )
+ *   );
+ * 
+ * </code>
+ * 
+ * Data Example 2: Use boolean values to show/hide blocks 
+ * 
+ * <code>
+ *   $tpl->parse(array(
+ *       'headTitle' => 'This is the Head Title.',
+ *       'pageTitle' => 'This is the main page title.',
+ *       'rowBlock' => true,
+ *       'dataBlock' => true,
+ *       'dataValue' => 'This is a fuckin test'
+ *     )
+ *   )
+ * </code>
+ *
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ * @todo Add caching ability
+ */
+class CurlyTemplate extends Object
+{
+    
+    /**
+     * @var string
+     */
+    protected $ld = '{';
+
+    /**
+     * @var string
+     */
+    protected $rd = '}';
+
+    /**
+     * @var string
+     */
+    protected $template = '';
+
+
+    /**
+     * CurlyTemplate constructor.
+     * @param string $template
+     */
+    public function __construct($template)
+    {
+        $this->template = $template;
+    }
+
+
+    /**
+     * Create a CurlyTemplate object
+     *
+     * @param string $template
+     * @return CurlyTemplate
+     */
+    static function create($template)
+    {
+        $tpl = new static($template);
+        
+        return $tpl;
+    }
+
+    /**
+     * Parse the template with the supplied data.
+     * 
+     * This is a mutable object and parse() can be called multiple times with 
+     * different data collections without issue.
+     * 
+     * 
+     * @param array $data
+     * @return string
+     */
+    function parse($data)
+    {
+        $template = $this->template;
+        $template = $this->parseRecursive($template, $data);
+        $template = $this->parseBlock($template, $data);
+        return $template;
+    }
+
+    /**
+     * Parse a block and replace all curly variable with their
+     * appropriate data value from the data array
+     *
+     * @param string $str
+     * @param array $data
+     * @return string
+     */
+    private function parseBlock($str, $data)
+    {
+        if (!is_array($data)) return $str;
+        foreach($data as $k => $v) {
+            if (!is_string($v)) continue;
+            $str = str_replace($this->ld . $k . $this->rd, $v, $str);
+        }
+        return $str;
+    }
+
+    /**
+     * @param string $template
+     * @param array $data
+     * @return string
+     *
+     * @todo Add escape delimiters '{{' and '}}
+     */
+    private function parseRecursive($template, $data = null)
+    {
+        $ld = preg_quote($this->ld);
+        $rd = preg_quote($this->rd);
+        
+        // original regex, not working?
+        //$reg = '#' . $ld . '([^' . $ld . ']*?)' .  $rd . '((?:[^{]|{(?!/?(.+)})|(?R))+)' . $ld . '[^' . $ld . ']\1' . $rd . '#si';
+        
+        // My interpretation that seems to work
+        $reg = '#' . $ld . '([^' . $ld . ']*?)' .  $rd . '(.*?)' . $ld . '[^' . $ld . ']\1' . $rd . '#si';
+        $template = preg_replace_callback($reg, function ($matches) use ($data) {
+            $tplData = $data;
+            if (isset($matches[0])) {
+                $tpl = $matches[2];
+                $name = $matches[1];
+                if (isset($tplData[$name])) {
+                    if (isset($tplData[$name][0])) {
+                        $block = '';
+                        foreach($tplData[$name] as $rowData) {
+                            $block .= $this->parseRecursive($tpl, $rowData);
+                        }
+                        return $block;
+                    } else {
+                        return $this->parseRecursive($tpl, $tplData[$name]);
+                    }
+                }
+                return $this->parseRecursive($tpl, $tplData);
+            }
+            return '';
+        }, $template);
+        return $this->parseBlock($template, $data);
+    }
+
+    /**
+     *  Set the left/right variable delimiters
+     *
+     * @param string $ld Left Delimiter
+     * @param string $rd Right Delimiter
+     * @return $this
+     */
+    public function setDelimiters($ld = '{', $rd = '}')
+    {
+        $this->ld = $ld;
+        $this->rd = $rd;
+        return $this;
+    }
+
+
+
+}
