f117b39b4f98821a9b94961facc2ecd181e6b9e9
mifsudm <mifsudm@unimelb.edu.au>
2017-08-29 12:49:55 +1000

    Minor Code Updates - 252s-dev

--- functions/varDump.php
+++ functions/varDump.php
@@ -17,7 +17,8 @@ function vd() {
     $str = sprintf('vd(%s [%s])', $path, $line['line']) . "\n";
     $str .= \Tk\Debug\VarDump::getInstance($config->getSitePath())->makeDump(func_get_args());
 
-    if (!$config->isCli()) {
+    //if (!$config->isCli()) {
+    if (!$log instanceof \Psr\Log\NullLogger) {
         $log->info($str);
     } else {
         error_log($str);
@@ -35,14 +36,15 @@ function vdd() {
     $config = \Tk\Config::getInstance();
     $line = current(debug_backtrace());
     /* @var \Psr\Log\LoggerInterface $log */
-    $log =  $config->getLog();
+    $log = $config->getLog();
     if (!$log) $log = new \Psr\Log\NullLogger();
 
     $path = str_replace($config->getSitePath(), '', $line['file']);
     $str = sprintf('vdd(%s [%s])', $path, $line['line']) . "\n";
     $str .= \Tk\Debug\VarDump::getInstance($config->getSitePath())->makeDump(func_get_args(), true);
 
-    if (!$config->isCli()) {
+    //if (!$config->isCli()) {
+    if (!$log instanceof \Psr\Log\NullLogger) {
         $log->info($str);
     } else {
         error_log($str);
