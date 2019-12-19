0e626bde5c3c99a628243f2a63d335d6cc9b6367
mifsudm <mifsudm@unimelb.edu.au>
2019-12-20 05:27:57 +1100

    ~Auto: Commit

---
+++ Tk/ConfigTrait.php
@@ -0,0 +1,62 @@
+<?php
+namespace Tk;
+
+
+/**
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2019 Michael Mifsud
+ */
+trait ConfigTrait
+{
+
+    /**
+     * @return Config|\Bs\Config|\Uni\Config|\App\Config
+     */
+    public function getConfig()
+    {
+        return Config::getInstance();
+    }
+
+    /**
+     * @param string $name
+     * @return \PDO|Db\Pdo|null
+     */
+    public function getDb($name = 'db')
+    {
+        return $this->getConfig()->getDb($name);
+    }
+
+    /**
+     * @return Request
+     */
+    public function getRequest()
+    {
+        return $this->getConfig()->getRequest();
+    }
+
+    /**
+     * @return Session
+     */
+    public function getSession()
+    {
+        return $this->getConfig()->getSession();
+    }
+
+    /**
+     * @return Db\User|\Uni\Db\User|\App\Db\User
+     */
+    public function getUser()
+    {
+        return $this->getConfig()->getUser();
+    }
+
+    /**
+     * @return Uri
+     */
+    public function getBackUrl()
+    {
+        return $this->getConfig()->getBackUrl();
+    }
+
+}
