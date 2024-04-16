d439ce358b02aea918cc9aec243cf1faa822cbaa
tropotek <info@tropotek.com>
2024-04-16 15:04:27 +1000

    Fix wiki menu edit page_id api error

--- Tk/Db/Mapper/Result.php
+++ Tk/Db/Mapper/Result.php
@@ -53,7 +53,7 @@ class Result implements \Iterator, \Countable
         $obj->statement = $statement;
         $tool = $tool ?? new Tool();
         $obj->tool = $tool;
-        [$_limit, $_offset, $total] = $mapper->getDb()->countFoundRows($statement->queryString, $statement->getBindParams());
+        [$_limit, $_offset, $total] = $mapper->getDb()->countFoundRows($statement->queryString, $statement->getBindParams() ?? []);
         $tool->setFoundRows($total);
         $obj->foundRows = $tool->getFoundRows();
         return $obj;
