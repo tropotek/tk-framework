534e32e2856fead9c50d84dfe261a726ce56308a
tropotek <info@tropotek.com>
2024-10-08 13:00:50 +1100

    Update discover baseUrl code

--- Tk/System.php
+++ Tk/System.php
@@ -17,19 +17,80 @@ class System
     }
 
     /**
-     * Attempt to locate .htaccess and find a RewriteBase parameter to use
+     * Discover the site's base URL.
      */
     public static function discoverBaseUrl(): string
     {
-        $path = '/';
-        $htaccessFile = self::discoverBasePath() . '/.htaccess';
-        if (is_file($htaccessFile)) {
-            $htaccess = file_get_contents($htaccessFile);
-            if ($htaccess && preg_match('/\s+RewriteBase (\/.*)\s+/i', $htaccess, $regs)) {
-                $path = $regs[1] ?? '';
+        $filename = basename($_SERVER['SCRIPT_FILENAME'] ?? '');
+
+        if (basename($_SERVER['SCRIPT_NAME'] ?? '') === $filename) {
+            $baseUrl = $_SERVER['SCRIPT_NAME'];
+        } elseif (basename($_SERVER['PHP_SELF'] ?? '') === $filename) {
+            $baseUrl = $_SERVER['PHP_SELF'];
+        } else {
+            // Backtrack up the script_filename to find the portion matching
+            // php_self
+            $path = $_SERVER['PHP_SELF'] ?? '';
+            $file = $_SERVER['SCRIPT_FILENAME'] ?? '';
+            $segs = explode('/', trim($file, '/'));
+            $segs = array_reverse($segs);
+            $index = 0;
+            $last = \count($segs);
+            $baseUrl = '';
+            do {
+                $seg = $segs[$index];
+                $baseUrl = '/'.$seg.$baseUrl;
+                ++$index;
+            } while ($last > $index && (false !== $pos = strpos($path, $baseUrl)) && 0 != $pos);
+        }
+
+        // Does the baseUrl have anything in common with the request_uri?
+        $requestUri = $_SERVER['REQUEST_URI'];
+        if ('' !== $requestUri && '/' !== $requestUri[0]) {
+            $requestUri = '/'.$requestUri;
+        }
+
+        if ($baseUrl && null !== $prefix = self::getUrlencodedPrefix($requestUri, $baseUrl)) {
+            // full $baseUrl matches
+            $baseUrl = $prefix;
+        } else if ($baseUrl && null !== $prefix = self::getUrlencodedPrefix($requestUri, rtrim(\dirname($baseUrl), '/'.\DIRECTORY_SEPARATOR).'/')) {
+            // directory portion of $baseUrl matches
+            $baseUrl = rtrim($prefix, '/'.\DIRECTORY_SEPARATOR);
+        } else {
+            $truncatedRequestUri = $requestUri;
+            if (false !== $pos = strpos($requestUri, '?')) {
+                $truncatedRequestUri = substr($requestUri, 0, $pos);
+            }
+            $basename = basename($baseUrl ?? '');
+            if (empty($basename) || !strpos(rawurldecode($truncatedRequestUri), $basename)) {
+                // no match whatsoever; set it blank
+                $baseUrl = '';
+            }
+
+            // If using mod_rewrite or ISAPI_Rewrite strip the script filename
+            // out of baseUrl. $pos !== 0 makes sure it is not matching a value
+            // from PATH_INFO or QUERY_STRING
+            if (\strlen($requestUri) >= \strlen($baseUrl) && (false !== $pos = strpos($requestUri, $baseUrl)) && 0 !== $pos) {
+                $baseUrl = substr($requestUri, 0, $pos + \strlen($baseUrl));
             }
         }
-        return rtrim($path, '/');
+
+        return rtrim($baseUrl, '/'.\DIRECTORY_SEPARATOR);
+    }
+
+
+    /**
+     * Returns the prefix as encoded in the string when the string starts with
+     * the given prefix, null otherwise.
+     */
+    private static function getUrlencodedPrefix(string $string, string $prefix): ?string
+    {
+        if (!str_starts_with(rawurldecode($string), $prefix)) return null;
+        $len = strlen($prefix);
+        if (preg_match(sprintf('#^(%%[[:xdigit:]]{2}|.){%d}#', $len), $string, $match)) {
+            return $match[0];
+        }
+        return null;
     }
 
     /**
