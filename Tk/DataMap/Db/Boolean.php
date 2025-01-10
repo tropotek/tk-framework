1f3c30064f7be3fa3a5a90e5f10e40da9568fdfe
tropotek <info@tropotek.com>
2025-01-11 06:03:02 +1100

    ~Auto: Commit

--- Tk/DataMap/Db/Boolean.php
+++ Tk/DataMap/Db/Boolean.php
@@ -12,22 +12,28 @@ class Boolean extends DataTypeInterface
     public function getPropertyValue(array $array): mixed
     {
         $value = parent::getPropertyValue($array);
+
         if (!(is_null($value) || is_bool($value))) {
-            $value = (
-                $value == $this->getColumn() ||
-                $value == $this->getProperty() ||
-                strtolower($value) == 'yes' ||
-                strtolower($value) == 'true' ||
-                (int)$value == 1
-            );
+            if ($value == $this->getColumn() || $value == $this->getProperty()) {
+                $value = true;
+            }
+
+            if ($value == '') $value = false;
+
+            $value = truefalse($value);
         }
+
         return $value;
     }
 
     public function getColumnValue(object $object): mixed
     {
         $value = parent::getColumnValue($object);
-        if (!is_null($value)) $value = (int)$value;
+
+        if (!is_null($value)) {
+            $value = (int)$value;
+        }
+
         return $value;
     }
 
--- Tk/DataMap/Form/Boolean.php
+++ Tk/DataMap/Form/Boolean.php
@@ -12,22 +12,28 @@ class Boolean extends DataTypeInterface
     public function getPropertyValue(array $array): ?bool
     {
         $value = parent::getPropertyValue($array);
+
         if (!(is_null($value) || is_bool($value))) {
             if ($value == $this->getColumn() || $value == $this->getProperty()) {
                 $value = true;
-            } else {
-                $value = truefalse($value);
             }
+
+            if ($value == '') $value = false;
+
+            $value = truefalse($value);
         }
+
         return $value;
     }
 
     public function getColumnValue(object $object): string
     {
         $value = parent::getColumnValue($object);
+
         if (!is_null($value)) {
             $value = strval(intval($value));    // use 1 = true, 0 = false
         }
+
         return $value;
     }
 
